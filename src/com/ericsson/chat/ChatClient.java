eefe01c6604c097ed48cbf0117c8928cdc7c9d04
ezzissn <kim.sun@ericsson.com>
2017-03-23 00:07:44 +0800

    version_0.2

--- src/com/ericsson/chat/ChatClient.java
+++ src/com/ericsson/chat/ChatClient.java
@@ -1,9 +1,15 @@
 package com.ericsson.chat;
 
+import java.awt.BorderLayout;
 import java.awt.Frame;
+import java.awt.TextArea;
+import java.awt.TextField;
 
 public class ChatClient extends Frame {
 
+	TextField tfText = new TextField();
+	TextArea taContent = new TextArea();
+	
 	public static void main(String[] args) {
 		new ChatClient().lunchFrame();
 	}
@@ -11,6 +17,11 @@ public class ChatClient extends Frame {
 	public void lunchFrame() {
 		this.setLocation(400,  300);
 		this.setSize(300, 300);
+		
+		this.add(tfText, BorderLayout.SOUTH);
+		this.add(taContent, BorderLayout.NORTH);
+		this.pack();
+		
 		this.setVisible(true);
 	}
 	
